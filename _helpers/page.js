8b629953c5ce16614e8675f271260f77d9bd2c8d
Eric Cornelissen <ericornelissen@gmail.com>
2018-09-14 11:40:07 +0200

    Use the page helper for both setting and getting

--- _helpers/page.js
+++ _helpers/page.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const utils = require('handlebars-utils');
 
-module.exports = function(name, value, options) {
-  this.page = this.page || {};
-  this.page[name] = value;
 
-  // Find the relative URL for this file
-  let cwd = options.data.file.cwd;
-  let filepath = options.data.file.path;
-  let fileext = path.extname(filepath);
-  this.page.url = filepath.slice(cwd.length, -fileext.length);
+function get(key, options, self) {
+  switch (key.toLowerCase()) {
+    case 'url':
+      let cwd = options.data.file.cwd;
+      let filepath = options.data.file.path;
+      let fileext = path.extname(filepath);
+      return filepath.slice(cwd.length, -fileext.length);
+    default:
+      return self.$page[key];
+  }
+}
+
+function set(key, value, self) {
+  self.$page = self.$page || { };
+  self.$page[key] = value
+}
+
+
+module.exports = function(key, value) {
+  if (utils.isOptions(value)) {
+    let options = value;
+    return get(key, options, this);
+  } else {
+    set(key, value, this);
+  }
 }
