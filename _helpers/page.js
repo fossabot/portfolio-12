eeb5237ca527e4166836a46f8c81ec7f71eb2f5c
Eric Cornelissen <ericornelissen@gmail.com>
2018-09-14 11:12:16 +0200

    Find relative URL for page

--- _helpers/page.js
+++ _helpers/page.js
@@ -1,4 +1,12 @@
-module.exports = function(name, value) {
+const path = require('path');
+
+module.exports = function(name, value, options) {
   this.page = this.page || {};
   this.page[name] = value;
+
+  // Find the relative URL for this file
+  let cwd = options.data.file.cwd;
+  let filepath = options.data.file.path;
+  let fileext = path.extname(filepath);
+  this.page.url = filepath.slice(cwd.length, -fileext.length);
 }
