cd6f270b0be7db894479ae5fa96bfd2cca789967
Eric Cornelissen <ericornelissen@gmail.com>
2018-10-07 13:00:45 +0300

    refactor: seperate intersection observer for <nav>

--- public/scripts/back-to-top.js
+++ public/scripts/back-to-top.js
@@ -3,7 +3,6 @@ let f = t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 // Elements
 let backToTop = document.querySelector('.back-to-top');
 let footer = document.querySelector('.footer');
-let navbarSocial = document.querySelector('.navbar-group-social');
 
 // Hide back to top and add click listener
 backToTop.classList.add('back-to-top--hidden');
@@ -21,19 +20,11 @@ backToTop.addEventListener('click', e => {
 // Observe the footer to determine if .back-to-top should be shown
 let observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
-    // Hide or show the "Back to top" button
     if(entry.isIntersecting && document.body.scrollTop > 50) {
       backToTop.classList.remove('back-to-top--hidden');
     } else {
       backToTop.classList.add('back-to-top--hidden');
     }
-
-    // Hide or show the
-    if (entry.intersectionRatio > 0.7) {
-      navbarSocial.classList.add('navbar__group--hidden');
-    } else {
-      navbarSocial.classList.remove('navbar__group--hidden');
-    }
   });
-}, { threshold: [0, 0.7] });
+}, { threshold: [0] });
 observer.observe(footer);
