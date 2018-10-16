3d0004cb2b4d664fa43f8aef61ca9a853f1ab8b6
Eric Cornelissen <ericornelissen@gmail.com>
2018-10-16 22:21:44 +0300

    chore: ignore "no-document-write" rule because of irrelevance and issues

--- .lighthouse.js
+++ .lighthouse.js
@@ -12,6 +12,7 @@ module.exports = {
       'consistently-interactive', // not working
       'estimated-input-latency', // not working
       'offscreen-images', // not working
+      'no-document-write', // not applicable, fails due to Browsersync
       'uses-http2', // covered by Cloudflare
       'manifest-short-name-length', // irrelevant for static site
       'link-text' // not interested
