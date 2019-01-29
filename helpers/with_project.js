4aad07778837df2f5ac2e162239b45612c5ae0d1
Eric Cornelissen <ericornelissen@gmail.com>
2019-01-29 15:12:09 +0200

    archive: re-add year markers for projects

--- helpers/with_project.js
+++ helpers/with_project.js
@@ -2,13 +2,20 @@
 
 const projects = require('../data/portfolio.json');
 
-module.exports = function(category, name, options) {
-  for (let project of projects[category]) {
-    if (project.name === name) {
-      this.project = project;
+module.exports = function(categoryId, projectName, options) {
+  for (let category of projects[categoryId]) {
+    if (category.items) {
+      for (let project of category.items) {
+        if (project.name === projectName) {
+          this.project = project;
+          return options.fn(this);
+        }
+      }
+    } else if (category.name === projectName) {
+      this.project = category;
       return options.fn(this);
     }
   }
 
-  throw new Error(`Project with name "${name}" not found`);
+  throw new Error(`Project with name "${projectName}" not found`);
 };
