e89a99984fe9ecd8ae2e115432e8fc713010dd66
Eric Cornelissen <ericornelissen@gmail.com>
2019-04-06 15:22:33 +0300

    feat: proof of concept event pages

--- helpers/with_project.js
+++ helpers/with_project.js
@@ -1,12 +1,23 @@
 /* jshint node: true */
 
 const contributions = require('../data/contributions.json');
+const events = require('../data/timeline.json');
 const projects = require('../data/projects.json');
 const snippets = require('../data/snippets.json');
 
 let categories = {contributions, projects, snippets};
 
 module.exports = function(categoryName, projectName, options) {
+  if (categoryName === 'event') {
+    for (let event of events) {
+      if (event.title === projectName) {
+        console.log(event);
+        this.project = event;
+        return options.fn(this);
+      }
+    }
+  }
+
   let category = categories[categoryName];
   for (let projects of Object.values(category)) {
     for (let project of projects) {
