2b7252b1173f87f0d8a1c4b5b2a84149443da33c
Eric Cornelisesn <ericornelissen@gmail.com>
2020-06-24 12:41:52 +0300

    fix: maintain scroll position on reload
    while maintaining the functionality of back-to-top. In the previous commit a style
    was added to the <html> element to make the back-to-top element work. This has now
    been removed and instead the back-to-top functionality is now based on
    `document.documentElement` instead of `document.body`.

--- www/scripts/back-to-top.js
+++ www/scripts/back-to-top.js
@@ -13,15 +13,15 @@ backToTop.addEventListener('click', e => {
   let t = 0;
   let interval = setInterval(function() {
     t += 0.015;
-    document.body.scrollTop -= document.body.scrollTop * f(t);
-    if(document.body.scrollTop <= 0) clearInterval(interval);
+    document.documentElement.scrollTop -= document.documentElement.scrollTop * f(t);
+    if(document.documentElement.scrollTop <= 0) clearInterval(interval);
   }, 10);
 });
 
 // Observe the footer to determine if .back-to-top should be shown
 let observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting && document.body.scrollTop > 50) {
+    if(entry.isIntersecting && document.documentElement.scrollTop > 50) {
       backToTop.classList.remove('back-to-top--hidden');
     } else {
       backToTop.classList.add('back-to-top--hidden');
